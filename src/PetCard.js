e61ddf5ccd167fe9de237db2c668740b4aed47a3
hoonkim <hoonk9387@gmail.com>
2022-11-30 20:43:41 -0600

    Added ternary expression in petacard for conditional rendering.

--- src/PetCard.js
+++ src/PetCard.js
@@ -11,7 +11,8 @@ Using the isAdopted prop, conditionally render the EmojiButton
 function PetCard({ 
   name, 
   image = "https://upload.wikimedia.org/wikipedia/en/5/5f/Original_Doge_meme.jpg", 
-  favSnacks 
+  favSnacks,
+  isAdopted
 }) {
   
   const snackLis = favSnacks.map(snack => <li key={snack}>{snack}</li>)
@@ -20,7 +21,8 @@ function PetCard({
     <div className="card">
       <h2>{name}</h2>
       <img src={image} alt={name} />
-      <EmojiButton label="Adopt" />
+      {/* <EmojiButton label="Adopt" /> */}
+      {!isAdopted ? <EmojiButton label="Adopt" /> : null}
       <h3>Favorite Snacks</h3>
       <ul>
         {snackLis}
